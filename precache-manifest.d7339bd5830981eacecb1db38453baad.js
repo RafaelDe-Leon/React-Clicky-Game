a265b127bc8597a80fbf5bdb086ef474fa7812d8
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-12-04 00:20:32 -0500

    Updates

---
+++ precache-manifest.d7339bd5830981eacecb1db38453baad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4342713d275b3379005c47a9f353698",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "b994eddff67aea22bbcd",
+    "url": "/React-Clicky-Game/static/css/main.9c90a3c7.chunk.css"
+  },
+  {
+    "revision": "8a20fc7804ea6b1efb9c",
+    "url": "/React-Clicky-Game/static/js/2.f3e0396d.chunk.js"
+  },
+  {
+    "revision": "b994eddff67aea22bbcd",
+    "url": "/React-Clicky-Game/static/js/main.d06ab0e4.chunk.js"
+  },
+  {
+    "revision": "9dcb7647e41138181938",
+    "url": "/React-Clicky-Game/static/js/runtime-main.44f57c79.js"
+  }
+]);
